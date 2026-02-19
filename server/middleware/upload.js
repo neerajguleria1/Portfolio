3abd3ba4cbe23370e32e28fd8b91de6bb49c30c5
Neeraj Guleria <neerajguleria30@gmail.com>
2026-02-19 18:22:56 +0530

    Fix CloudinaryStorage import error

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,7 +1,6 @@
 import multer from "multer";
 import { v2 as cloudinary } from "cloudinary";
-import pkg from "multer-storage-cloudinary";
-const { CloudinaryStorage } = pkg;
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 // Configure Cloudinary
 cloudinary.config({
